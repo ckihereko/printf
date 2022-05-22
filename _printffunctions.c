9fe37a704b6b23a3b1f3de3231d3bfa2f3fce0ff
Jeddy Mweru <jeddymweru@gmail.com>
2022-05-22 12:26:58 -0700

    Adding the _printf functions

---
+++ _printffunctions.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/*PRINTING CHARACTERS*/
+/**
+ * print_char - prints character to output
+ * @arg
+ * Return: 1 on succes
+ */
+int print_char(va_list arg)
+{
+
+	return (_putchar(va_arg(arg, int)));
+
+}
+/*PRINTING STRINGS*/
+/**
+ * print_str - prints a string
+ *
+ * Return: number of chacters printed
+ */
+int print_str(va_list arg)
+{
+	int count;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+	_putchar(str[count]);
+	}
+	return (count);
+}
+/*PRINTING integers*/
+/**
+ * print_ int - prints integer
+ * @arg: integer argument
+ * Return: 0
+ */
+int print_int(va_list arg)
+{
+	int digit = va_arg(arg, int);
+	unsigned int a = 1, i, result, charp = 0;
+
+	for (i = 0; digit / a > 9; i++, a *= 10)
+	;
+	for (; divisor >= 1; n %= a, a /= 10, charp++)
+	{
+		result = digit / a;
+		putchar('0' + result);	
+	}
+	return (charp);
+
+}
