17116909767e73bb5d6978023638d438b70de4e5
ckihereko <102221386+ckihereko@users.noreply.github.com>
2022-05-24 16:31:43 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,85 +1,48 @@
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
-# include "main.h"
+#include "main.h"
 
 /**
- * printSpecifier - prints the specifiers c,d ,i
- * @spec: argument to the function is the specifier
- * @arg: the arguments which are specifers
- * Return: number of charcters printed
- */
-int printSpecifier(char spec, va_list arg)
-{
-	unsigned int i;
-
-
-	specifierStruct sp[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"d", print_int},
-		{"i", print_int},
-		{NULL, NULL}
-	};
-
-	for (i = 0; sp[i].a != NULL; i++)
-	{
-
-		if (sp[i].a[0] == spec)
-		{
-			return (sp[i].print(arg));
-		}
-
-	}
-	return (0);
-}
-
-/**
- * _printf - prints anything
- * @format: arguments passed
- * Return: number of characters printed
- * (excluding the null byte used to end output to strings)
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	unsigned int j;
-	int count = 0;
-	int specifierPrinted = 0;
+    int (*pfunc)(va_list, flags_t *);
+    const char *p;
+    va_list arguments;
+    flags_t flags = {0, 0, 0};
 
-	va_list arg;
-	/*start of  iterations in the variadic function arguments*/
-	va_start(arg, format);
-	if (format == NULL)
-		return (-1);
-	for (j = 0; format[j] != '\0'; j++)
-	{
-		if (format[j] != '%')
-		{
-			_putchar(format[j]);
-			count++;
-			continue;
-		}
-		if (format[j + 1] == '%')
-		{
-			_putchar('%');
-			count++;
-			continue;
-		}
-		if (format[j + 1] == '\0')
-			return (-1);
-		specifierPrinted = printSpecifier(format[j + 1], arg);
-		if (specifierPrinted == 0)
-		{
-			_putchar('%');
-			count++;
-		}
-		if (specifierPrinted > 0)
-		{
-			count = count + specifierPrinted;
-		}
-		if (specifierPrinted == -1 || specifierPrinted != 0)
-			count++;
-	}
-	va_end(arg);
-	return (count);
+    register int count = 0;
+
+    va_start(arguments, format);
+    if (!format || (format[0] == '%' && !format[1]))
+        return (-1);
+    if (format[0] == '%' && format[1] == ' ' && !format[2])
+        return (-1);
+    for (p = format; *p; p++)
+    {
+        if (*p == '%')
+        {
+            p++;
+            if (*p == '%')
+            {
+                count += _putchar('%');
+                continue;
+            }
+            while (get_flag(*p, &flags))
+                p++;
+            pfunc = get_print(*p);
+            count += (pfunc)
+                         ? pfunc(arguments, &flags)
+                         : _printf("%%%c", *p);
+        }
+        else
+            count += _putchar(*p);
+    }
+    _putchar(-1);
+    va_end(arguments);
+    return (count);
 }
